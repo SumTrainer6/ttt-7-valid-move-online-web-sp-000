9502db5b58098ad7d57c6f2ae5d25747298c0dc8
summer trainer <summertrainer@yahoo.com>
2021-06-28 00:44:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
- 
+# check if position is in range of board
+  if position == 
+# check if position is not taken
+
 end 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 # def position_taken?(board, position)
