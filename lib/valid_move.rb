36c08c805e95a0720a1fceffaae5470b340f7efb
summer trainer <summertrainer@yahoo.com>
2021-06-28 00:23:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,9 +8,9 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
   # binding.pry
-  if board[position] == " " || board[position] == "" || board[position] == nil 
-    taken = false
-  else
-    taken = true
-  end
-end
+  # if board[position] == " " || board[position] == "" || board[position] == nil 
+    # taken = false
+  # else
+    # taken = true
+  # end
+# end
