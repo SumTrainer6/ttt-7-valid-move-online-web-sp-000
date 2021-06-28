730d50cb5961576bb5b8a9c1a181a1f1fac01769
summer trainer <summertrainer@yahoo.com>
2021-06-28 00:17:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  if board[position] == (1 - 9)
+  elsif board[position] == int > 9
+end 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  # binding.pry
+  if board[position] == " " || board[position] == "" || board[position] == nil 
+    taken = false
+  else
+    taken = true
+  end
+end
